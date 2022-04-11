41aa76650039f38a39beb23d12b56a59fd02f049
ADMNYC <ADM@USA.COM>
2022-04-11 13:52:39 +0000

    Added Date.java

---
+++ pre02/Date.java
@@ -0,0 +1,15 @@
+javacimport java.io.*;
+import java.time.Year;
+import java.util.*;
+
+public class Date {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+String day = "Monday";
+String month = "April";
+String date = "5th";
+int year = 1972;
+      System.out.println(day +"," + month + " " +date +", " + year);
+          
+      }
+}
