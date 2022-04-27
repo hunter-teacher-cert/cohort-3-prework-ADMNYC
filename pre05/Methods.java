ddc9aa782311d8e2817cb5a3734759dce78f0a4a
ADMNYC <ADM@USA.COM>
2022-04-27 17:58:25 +0000

    pre05/Methods.java/Exercise 2 + Exercise 3

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -1,28 +1,48 @@
-/*Exercise 2  
-Write a method named isDivisible that takes two integers, n and m, and that returns true if n is divisible by m, and false otherwise.*/
-
-import java.util.Scanner;
+import java.util.*;
   
-public class Methods {
+class Methods {
     public static void isDivisible(int n, int m){
           if (n % m == 0) {      
             System.out.println((n % m == 0) + ".");
-            System.out.print (n + " is divible by "+ m);}
+            System.out.println (n + " is divible by "+ m);}
           else {
             System.out.println((n % m == 0) + ".");   
-            System.out.print (n + " is not divible by "+ m);}
-          
+            System.out.println (n + " is not divible by "+ m);}  
+    }
+    public static void isTriangle(int a, int b, int c){
+          if (a <=0 || b<=0 ||c<0) {System.out.println("Invalid entries. Please enter postive numbers only.");}
+          else if ((a + b > c) && ((Math.abs(a - b) < c))) {      
+            System.out.println(((a + b > c) && ((Math.abs(a - b) < c))) + ".");
+            System.out.print (a+", "+b+", " +c+ " are sides of a triangle.");
+            }
+          else{
+            System.out.println(((a + b > c) && ((Math.abs(a - b) < c))) + ".");   
+            System.out.print (a+", "+b+", " +c+ " are NOT sides of a triangle.");
+            }    
+  
     }
    
     public static void main (String[] args) {
       Scanner keyBoard = new Scanner(System.in);
-      
-      System.out.print ("Please enter a number: ");
-      int n = keyBoard.nextInt();
-           
-      System.out.print ("Please enter another number: ");
-      int m = keyBoard.nextInt();
-         
+      System.out.print ("\n\nExercise 2: isDivisible Method \n\n");
+      System.out.print ("Please enter a number (dividend): ");
+      int n = keyBoard.nextInt();           
+      System.out.print ("Please enter another number (divisor): ");
+      int m = keyBoard.nextInt();         
       isDivisible(n, m);
+
+      System.out.print ("\n\nExercise 3: isTriangle Method \n\n");
+      System.out.print ("Please enter a number (side 1): ");
+      int a = keyBoard.nextInt();          
+      System.out.print ("Please enter another number (side 2): ");
+      int b = keyBoard.nextInt();
+      System.out.print ("Please enter another number (side 3): ");
+      int c = keyBoard.nextInt();         
+      isTriangle(a, b, c);
+ 
     }
 }
+    
+  
+    
+    
