feb7219acb5692f1bb3bbdaeed0427c0e8a33e81
ADMNYC <ADM@USA.COM>
2022-05-04 13:46:20 +0000

    pre06/PowerMethod added

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -8,11 +8,27 @@ class Loops {
     x1 =(x0 + (a/x0)) / 2;}
     System.out.println("The square root of "+ a + " is + " + x1);
    }
-    
+
+    public static void power(double x, int n){
+    int i = 1;
+    double y = x;
+     while (i<=n-1){x=x*y;
+     i++;}
+    System.out.println(x);
+      }
+  
   public static void main(String args[]){
          System.out.print("Please enter a number you'd like to take the square root of. ");  
         Scanner keyBoard = new Scanner(System.in);
         double a = keyBoard.nextInt();
         squareRoot(a);
+   
+
+      System.out.print("Please enter the base of the exponential expression. ");          
+       double x = keyBoard.nextDouble();
+        System.out.print("Please enter the exponent "+x+ " is raised to. ");         
+        int n = keyBoard.nextInt();
+        power(x, n);
+    
          }
 }
